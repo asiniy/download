0fb68cb65c66b3573a8a9c043e856e7696d255a6
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-07-17 10:09:55 +0700

    spin up a local server in tests

--- lib/download.ex
+++ lib/download.ex
@@ -116,8 +116,6 @@ defmodule Download do
 
   defp handle_async_response_chunk(%AsyncEnd{}, opts), do: finish_download({ :ok }, opts)
 
-  # Uncomment one line below if you are prefer to test not "Content-Length" header response, but a real file size
-  # defp do_handle_content_length(_, opts), do: do_download(opts)
   defp do_handle_content_length({ "Content-Length", content_length }, opts) do
     if String.to_integer(content_length) > opts.max_file_size do
       finish_download({ :error, :file_size_is_too_big }, opts)
